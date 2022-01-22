fc902eea3fd818b125c9b9e60078e953bcbc7064
5MayRain <zgh2606@gmail.com>
2022-01-22 23:25:29 +0800

    Site updated: 2022-01-22 23:25:27

--- js/diaspora.js
+++ js/diaspora.js
@@ -632,6 +632,6 @@ $(function() {
         comment.click();
     }
 		
-    console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/Fechin/hexo-theme-diaspora")
+    //console.log("%c Github %c","background:#24272A; color:#ffffff","","https://github.com/Fechin/hexo-theme-diaspora")
 })
 
