1a5e35aab725d99c154562dd73de99fc71ea9c3f
5MayRain <zgh2606@gmail.com>
2023-11-22 02:36:46 +0800

    Site updated: 2023-11-22 02:36:46

--- js/codeCopy.js
+++ js/codeCopy.js
@@ -1,1 +1,12 @@
-window,document,new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}});
+/* 页面载入完成后，创建复制按钮 */
+!function (e, t, a) { 
+  /* code */
+  var initCopyCode = function(){
+    new ClipboardJS('.btn-copy', {
+        target: function(trigger) {
+            return trigger.nextElementSibling;
+        }
+    });
+  }
+  initCopyCode();
+}(window, document);
